05b3521d579b6bef9562c9e3115d088e8fc9c33b
klimsemikin <klimsemikin@gmail.com>
2023-04-05 17:29:34 +0300

    add skill scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_142706) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_142841) do
   create_table "interests", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
