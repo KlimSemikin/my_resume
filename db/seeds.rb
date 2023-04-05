44305b0c6ae861b1a6519f39355bf7cb876fbb4d
klimsemikin <klimsemikin@gmail.com>
2023-04-05 19:17:12 +0300

    add seeds for users and their interests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-10.times do
-  interes = Faker::Hobby.activity
-  Interest.create(name: interes)
+15.times do
+  interest = Faker::Hobby.activity
+  Interest.create(name: interest)
 end
 
-10.times do
+15.times do
   skill = Faker::Job.key_skill
   Skill.create(name: skill)
+end
+
+5.times do
+  name = Faker::Name.first_name
+  surname = Faker::Name.last_name
+  patronymic = Faker::Name.middle_name
+  fullname = "#{surname} #{name} #{patronymic}"
+  email = Faker::Internet.free_email(name: name)
+  age = rand(20..50)
+  nationality = Faker::Nation.nationality
+  country = Faker::Address.country
+  gender = Faker::Gender.binary_type.downcase
+
+  user = User.create(name:, surname:, patronymic:, fullname:, email:, age:, nationality:, country:, gender:)
+
+  user.interests = Interest.order("RANDOM()").limit(5)
+  user.skills = Skill.order("RANDOM()").limit(5)
 end
