731db16fa339f09fd61646da1097737942b2eb19
klimsemikin <klimsemikin@gmail.com>
2023-04-06 14:08:34 +0300

    add uniqueness of email to bd checks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_100448) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_110705) do
   create_table "interests", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_100448) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "user_interests", "interests"
