857fa1df105fe49e5c4b5b2514c9a4d142390ea7
klimsemikin <klimsemikin@gmail.com>
2023-04-07 14:37:08 +0300

    remove extra validations from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,5 @@ class User < ApplicationRecord
   has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
 
-  validates_presence_of :name, :surname, :patronymic, :email, :age, :nationality, :country, :gender
   validates :email, uniqueness: true
-  validates :age, numericality: { in: 1..90, only_integer: true }
-  validates :gender, inclusion: { in: genders.keys }
 end
